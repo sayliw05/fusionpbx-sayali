b3a997fc3cb567346b71b44a5ac76b337d4e34be
Sayali <sayali.wavhal@d10x.io>
2025-12-16 11:59:49 +0000

    Revert "Enhance database logging in config and database_handle scripts"
    This reverts commit 75d8ca6d126ad30ceb0877ba7b3c527822b8189d.

--- app/switch/resources/scripts/resources/functions/database_handle.lua
+++ app/switch/resources/scripts/resources/functions/database_handle.lua
@@ -2,10 +2,10 @@
 --connect to the database
 	function database_handle(t)
 		if (t == "system") then
-			freeswitch.consoleLog("notice", "[db_handle] Connecting to SYSTEM database: " .. tostring(database.system) .. "\n");
+			--freeswitch.consoleLog("notice","database.switch " .. database.system .. "\n");
 			return freeswitch.Dbh(database.system);
 		elseif (t == "switch") then
-			freeswitch.consoleLog("notice", "[db_handle] Connecting to SWITCH database: " .. tostring(database.switch) .. "\n");
+			--freeswitch.consoleLog("notice","database.switch " .. trim(database.switch) .. "\n");
 			return freeswitch.Dbh(database.switch);
 		end
 	end
